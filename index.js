87e6688ec77617f3638f1332ddbc82cae56db025
Kazi Md. Rakibul Hassan <trakib826@gmail.com>
2024-12-26 02:23:10 +0600

    routing and fs module

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("hello")
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+app.get('/',(req,res)=>{
+  // res.json({
+  //   "id": "1",
+  //   "message": "Hello User"
+  // })
+  // res.send(
+  //   `
+  //   <h1>Hello World</h1>
+  //   <p>Javascript is Awesome</p>
+  //   `
+  // )
+  fs.readFile('./pages/index.html',(err,data)=>{
+    if(err){
+      console.log("Error")
+      res.send(`
+        <h1>Something went wrong</h1>
+        `)
+    }else{
+      res.write(data)
+      res.end()
+    }
+  })
+})
+app.get('/about',(req,res)=>{
+  res.send(`
+    <h1>I am about route</h1>
+    `)
+})
+
+app.listen(5001,()=>{
+  console.log("Server is listening on port 5001")
+})

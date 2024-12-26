f7126138756c1eafcb2349b2230d4e801241f7eb
Kazi Md. Rakibul Hassan <trakib826@gmail.com>
2024-12-27 00:19:49 +0600

    global middleware and bad request

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const fs = require('fs')
+const morgan = require('morgan')
+const cors = require('cors')
+const { json } = require('stream/consumers')
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+app.use(cors())
+app.use(globalMiddleware)
 
 app.get('/',(req,res)=>{
   // res.json({
@@ -13,7 +22,7 @@ app.get('/',(req,res)=>{
   //   <p>Javascript is Awesome</p>
   //   `
   // )
-  console.log(req.get('Accept'))
+  // console.log(req.get('Accept'))
   fs.readFile('./pages/index.html',(err,data)=>{
     if(err){
       console.log("Error")
@@ -43,3 +52,14 @@ app.get('/about',(req,res)=>{
 app.listen(5001,()=>{
   console.log("Server is listening on port 5001")
 })
+
+function globalMiddleware(req,res,next){
+  console.log(`${req.method} - ${req.url}`)
+  console.log("I am a global middleware")
+
+  if(req.query.bad){
+    return res.status(400).send("Bad Request")
+  }
+
+  next()
+}
